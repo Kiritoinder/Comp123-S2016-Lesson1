8db1cbb6693070b537801bac2985c57aedfb2baf
Kiritoinder <inderbharaj@ymail.com>
2016-05-10 14:14:11 -0400

    Added hello world

--- Comp123-S2016lesson1/Program.cs
+++ Comp123-S2016lesson1/Program.cs
@@ -10,6 +10,21 @@ namespace Comp123_S2016lesson1
     {
         static void Main(string[] args)
         {
+            // call method
+            helloWorldToConsole();
+            
+        }
+        /**
+         * Method description
+         *This is my hello worls string
+         *return hellow world string
+         */
+
+        public static string helloWorldToConsole()
+        {
+            String helloWorldString = "Hello, World";
+            Console.WriteLine(helloWorldString);
+            return helloWorldString;
         }
     }
 }
